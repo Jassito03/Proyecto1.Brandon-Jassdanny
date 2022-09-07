43d30ec99c1ed47d4cc87c340e91ad1856630914
JOSEPH PI�AR BALTODANO <joseph.pinar.baltodano@est.una.ac.cr>
2022-09-07 13:50:16 -0600

    MCV all

---
+++ src/main/java/P/BJ/presentation/acercade/Controller.java
@@ -0,0 +1,21 @@
+package P.BJ.presentation.acercade;
+
+import P.BJ.Application;
+
+public class Controller {
+
+    Model model;
+    View view;
+
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+        view.setController(this);
+        view.setModel(model);
+    }
+
+    public void show(){
+        Application.window.setContentPane(view.getPanel());
+    }
+
+}
---
+++ src/main/java/P/BJ/presentation/empleado/Model.java
@@ -0,0 +1,13 @@
+package P.BJ.presentation.empleado;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Model extends Observable {
+
+    @Override
+    public void addObserver(Observer o){
+
+    }
+
+}
