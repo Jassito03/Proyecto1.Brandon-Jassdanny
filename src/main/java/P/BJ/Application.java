051d5422b7e483732d6f1523f5c62ecc07a1f424
JOSEPH PI�AR BALTODANO <joseph.pinar.baltodano@est.una.ac.cr>
2022-09-07 11:23:01 -0600

    main MVC

--- src/main/java/P/BJ/Application.java
+++ src/main/java/P/BJ/Application.java
@@ -1,4 +1,25 @@
 package P.BJ;
 
+import javax.swing.*;
+
 public class Application {
+    public static P.BJ.presentation.main.Controller controller;
+    public static JFrame window;
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception ex) {};
+
+        P.BJ.presentation.main.Model mainModel = new P.BJ.presentation.main.Model();
+        P.BJ.presentation.main.View mainView = new P.BJ.presentation.main.View();
+        controller = new P.BJ.presentation.main.Controller(mainModel, mainView);
+
+        window = new JFrame();
+        window.setSize(500,500);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setTitle("SISE: Sistema de Sucursales y Empleados");
+        window.setVisible(true);
+        controller.show();
+
+    }
 }
---
+++ src/main/java/P/BJ/presentation/main/View.java
@@ -0,0 +1,30 @@
+package P.BJ.presentation.main;
+
+import javax.swing.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class View implements Observer {
+
+    Model model;
+    Controller controller;
+    private JPanel panel;
+
+    public void setModel(Model model) {
+        this.model = model;
+        model.addObserver(this);
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    @Override
+    public void update(Observable updatedModel, Object param){
+
+    }
+}
